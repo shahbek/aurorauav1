786b22fc56e55771a1547a9d3cbaaa159c1958e0
ShahbekMiru <60500115+shahbek@users.noreply.github.com>
2022-10-20 12:03:16 -0700

    UI change
    added analytics screen
    glass card ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from '@mui/material/Button';
 import React, { useState } from "react";
 import { Layout, Container, BoxUpload, ImagePreview } from "./style";
 import FolderIcon from "./assets/folder_icon_transparent.png";
 import CloseIcon from "./assets/CloseIcon.svg";
 import {HelpCircle} from "react-feather";
+import {SpinnerDotted} from "spinners-react";
+import $ from "jquery";
+import Results from "./Results.json";
+import { PieChart } from 'react-minimal-pie-chart';
+
+
+
+
+function spinnerToggle(){
+ document.getElementById("process").style.display ="none";
+ $("#spinner").show();
+ $("#Analytics").show();
+ 
+}
 
 function App() {
 
   const [image, setImage] = useState("");
   const [isUploaded, setIsUploaded] = useState(false);
   const [typeFile, setTypeFile] = useState("");
+
+
+
 
   function handleImageChange(e) {
     if (e.target.files && e.target.files[0]) {
@@ -25,6 +41,15 @@ function App() {
 
       reader.readAsDataURL(e.target.files[0]);
     }
+  }
+
+  function printResults(result){
+      
+      for(let i = 0; i <= result.detections.length; i++){
+       return <h1>Species: {result.detections[i].label}</h1>;
+       
+      }
+    
   }
 
   return (
@@ -41,10 +66,10 @@ function App() {
         
 
       <Layout>
-      <Container>
+      <Container style={{backdropFilter: "blur(7px)", borderLeft: "solid 0.5px rgba(255, 255, 255, 0.3)",  borderTop: "solid 0.5px rgba(255, 255, 255, 0.3)"}}>
         <h2>Upload image of Tree or Forest Area</h2>
 
-        <BoxUpload>
+        <BoxUpload style={{backdropFilter: "blur(1px)"}}>
           <div className="image-upload">
             {!isUploaded ? (
               <>
@@ -104,14 +129,69 @@ function App() {
 
       
       <div style={{marginTop: "20px"}}>
-      <div><Button variant="contained">Generate</Button></div>
+
+      <div onClick={spinnerToggle} id="process" style={{backgroundColor:"white", width:"50%", margin:"0 auto", borderRadius:"20px", display:"block", cursor:"pointer"}}>
+          <p style={{color:"black", padding:"5px", fontWeight:"bold"}}>Process</p>
       </div>
-      <div><a style={{marginTop:"10px",color:"grey", fontSize: "15px"}}><HelpCircle size={15}/>Please only upload images of trees</a></div>
       
+      <div id="spinner" style={{display:"none"}}>    
+          <SpinnerDotted enabled={true} color={"white"} size={"35px"} thickness={150}/>
+      </div>
+
+
+      
+      </div>
+      <div><a style={{marginTop:"10px",color:"white", fontSize: "13px", fontWeight: "lighter"}}><HelpCircle size={15}/>Please only upload images of trees</a></div>
+      
+      
+
       </Container>
     </Layout>
     
-      
+    <Layout id="Analytics" style={{display: "none"}}>
+      <Container id="Analytics" style={{backdropFilter: "blur(7px)", borderLeft: "solid 0.5px rgba(255, 255, 255, 0.3)",  borderTop: "solid 0.5px rgba(255, 255, 255, 0.3)"}}>
+        <h1>Analytics</h1>
+        {
+          Results[0].detections && Results[0].detections.map( (result) => {
+            return <div key={result.id}>
+              <h3 key={result.id}>Species: {result.label}</h3>
+              <h4>    Confidence: {((result.score)*100).toFixed(2)}%</h4>
+                
+              </div>
+
+              
+            
+          })
+        }
+        {
+        Results[0].numDetections && Results[0].numDetections.map( (result) => {
+        
+          return <h4>Total trees: {result}</h4>
+        }
+        )}
+<Container style={{width: "90%", alignSelf: "center", background: "rgba(255,255,255,0.7)"}}>
+<PieChart
+  style={{width: "200px"}}
+  lineWidth={15}
+  paddingAngle = {15}
+  rounded = {true}
+  radius = {40}
+ 
+  viewBoxSize = {[100,100]}
+  center = {[50,50]}
+ 
+  data={[
+    { title: 'One', value: 10, color: '#14ccc0' },
+    { title: 'Two', value: 15, color: '#bcdc84' },
+    { title: 'Three', value: 20, color: '#74bc24' },
+    { title: 'Three', value: 20, color: '#04bcb4' },
+  ]}
+/>
+        </Container>
+      </Container>
+    </Layout>
+   
+  
       
     
     
--- src/style/index.js
+++ src/style/index.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const Layout = styled.div`
     height: 100vh;
@@ -6,19 +6,20 @@ export const Layout = styled.div`
 
     display: grid;
     place-items: center;
-
-    background: white;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-image: url("https://images.unsplash.com/photo-1618005198919-d3d4b5a92ead?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80");
 `
 
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-    background: #fff;
+    width: 50%;
     padding: 36px 48px;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.5) 0px 30px 60px -30px;
     border-radius: 20px;
     text-align: center;
-
+    background:rgba(255,255,255,0.3);
     p {
         margin-top: -10px;
         color: #777;
@@ -29,14 +30,14 @@ export const BoxUpload = styled.div`
     display: grid;
     margin-top: 20px;
     place-items: center;
-    border: 1px dashed grey;
+    border: 1px dashed white;
     /* padding: 36px 48px; */
     position: relative;
 
     height: 100%;
     width: 100%;
 
-    background: #FBFBFF;
+    background:rgba(255,255,255,0.05);
     border-radius: 20px;
 
     .image-upload {
@@ -69,14 +70,12 @@ export const ImagePreview = styled.div`
     }
 
     .close-icon{
-        background: #000;
-        border-radius: 5px;
-        opacity: .8;
-
+        opacity: .9;
+        height: 30px;
         position: absolute;
         z-index: 10;
         right: 15px;
-        top: 20px;
+        top: 15px;
         cursor: pointer;
 
         :hover {
